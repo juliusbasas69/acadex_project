b649d15d9bd0af776dc17b968da7ce63a9981dc4
JuliusB <julius.basas69@gmail.com>
2026-04-20 19:28:56 +0800

    return null if window is undefined

--- frontend/hooks/useAuth.ts
+++ frontend/hooks/useAuth.ts
@@ -44,6 +44,8 @@ export const useAuth = () => {
   };
 
   const getCurrentUser = (): User | null => {
+    if (typeof window === "undefined") return null;
+
     const token = document.cookie
       .split("; ")
       .find((row) => row.startsWith("token="))
